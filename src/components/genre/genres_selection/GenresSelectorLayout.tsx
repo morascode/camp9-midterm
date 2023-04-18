206ffebcd1d260d774dc76f1de60503ba1661057
intertemp <63150529+intertemp@users.noreply.github.com>
2023-04-18 12:22:28 +0200

    fixed issue with statemanagement of my Emoji components - thank you Mora! Fixed my counter to count up and down

--- src/components/genre/genres_selection/GenresSelectorLayout.tsx
+++ src/components/genre/genres_selection/GenresSelectorLayout.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import React, { useState } from 'react';
+import GenreIconButton from './GenreIconButton';
 
 export const EmojiLibary = [
   {
@@ -90,29 +91,16 @@ interface Props {
 }
 
 function GenresSelectorLayout(props: Props) {
-  const [isSelected, setIsSelected] = useState(false);
-
   return (
     <div className="grid grid-cols-4 gap-8">
       {EmojiLibary.map(param => (
-        <div
-          className="flex flex-col items-center gap-1"
-          onClick={() => {
-            props.setCounter(props.counter + 1);
-            setIsSelected(!isSelected);
-          }}
-          key={param.id}
-        >
-          <div
-            className={clsx(
-              'rounded-xl w-14 h-14 text-3xl flex flex-col justify-center items-center',
-              isSelected ? 'bg-white/40' : 'bg-[#363740]'
-            )}
-          >
-            <p>{param.Emoji}</p>
-          </div>
-          <h3 className="text-xs font-bold text-white/40">{param.Genre}</h3>
-        </div>
+        <GenreIconButton
+          emoji={param.Emoji}
+          genre={param.Genre}
+          id={param.id}
+          counter={props.counter}
+          setCounter={props.setCounter}
+        ></GenreIconButton>
       ))}
     </div>
   );
