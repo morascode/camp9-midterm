3920dd328b709978ac68dd717d5d1f7a65fc02c0
Doug <dmhwebb@gmail.com>
2023-04-25 11:36:23 +0200

    reformat, remove unneeded comment and unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
-import WelcomeHeader from './components/WelcomeHeader';
 import NavigationLayout from './components/NavigationLayout';
 import MovieDetails from './pages/MovieDetails';
 import BookDateAndTime from './pages/BookDateAndTime';
 import Home from './pages/Home';
 import Genres from './pages/Genres';
 import Credits from './pages/Credits';
-import SearchBar from './components/Searchbar';
 import LogInPage from './pages/LogInPage';
 import Movies from './pages/Movies';
 import EmojieProvider from './Context/GenreContext';
@@ -21,7 +19,7 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home/>,
+        element: <Home />,
       },
       {
         path: '/movies',
@@ -31,11 +29,11 @@ export const router = createBrowserRouter([
   },
   {
     path: '/login',
-    element: <LogInPage />, // insert your page here
+    element: <LogInPage />,
   },
   {
     path: '/genres',
-    element: <Genres/>, // insert your page here
+    element: <Genres />,
   },
   {
     path: '/movies/:id',
@@ -47,7 +45,7 @@ export const router = createBrowserRouter([
   },
   {
     path: '/dates',
-    element: <BookDateAndTime />, // insert your page here
+    element: <BookDateAndTime />,
   },
   {
     path: '/seats',
@@ -61,8 +59,8 @@ export const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-  <EmojieProvider>
-    <RouterProvider router={router} />
+    <EmojieProvider>
+      <RouterProvider router={router} />
     </EmojieProvider>
   </React.StrictMode>
 );
