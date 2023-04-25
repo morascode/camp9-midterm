6409512d70c8222d0d83cbe8b1506efe0d457f5c
mateusmorgado <mateusmorgado@hotmail.com>
2023-04-25 10:33:58 +0200

    update

--- src/pages/Ticket.tsx
+++ src/pages/Ticket.tsx
@@ -21,8 +21,8 @@ function Ticket() {
   
   
     return (
-              <div className="  grid place-items-center pt-8 px-5 ">
-                <div className="bg-[#494952] debug min-h-screen	 rounded-[12px] items-center ">
+              <div className=" grid place-items-center pt-8 px-5 ">
+                <div className="bg-[#494952] min-h-screen	 rounded-[12px] items-center ">
                   <div className=" ">
                     <img src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`} alt="Movie Poster" className=" top-0 rounded-t-[12px]  object-cover w-full h-[160px]" />
                   </div>
@@ -31,15 +31,15 @@ function Ticket() {
                       <h2 className="typography-title pt-2 pb-6 px-6">{data.title}</h2>
                 
                     </div>
-                    <div className="flex px-6 flex-c-3" >
-                      <div className="grid"><span className="debug">Date</span><br /><p className="debug">08 jan</p></div>
-                      <div className="grid"><span>Time</span><p>12:30</p></div>
-                      <div className="grid"><span>Price</span><p>56,00</p></div>
-                      <div className="grid"><span>Seats</span><p>c-3,c-4,c-5</p></div>
+                    <div className="flex px-6 " >
+                      <div className="grid"><span className="">Date</span><br/><p className="">08 jan</p></div>
+                      <div className="grid"><span className="">Time</span><p className="">12:30</p></div>
+                      <div className="grid"><span className="">Price</span><p className="">56,00</p></div>
+                      <div className="grid"><span className="">Seats</span ><p className="">c-3,c-4,c-5</p></div>
                     </div>
                     
                   </div>
-                  
+                  <div className="">Barcode</div>
                 </div>
                 <div className=" debug w-[335px] py-4"><Button className=" 	">Back To Home</Button></div>
               </div>
