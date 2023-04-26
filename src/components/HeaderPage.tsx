8d699cad666f402c4a77045a533a87503fdc790f
ehidne-mora <mora@slime.garden>
2023-04-26 14:52:24 +0200

    added comments and README.md

--- src/components/HeaderPage.tsx
+++ src/components/HeaderPage.tsx
@@ -8,6 +8,13 @@ interface Props {
   onHeartButtonClick?: () => void;
 }
 
+// This is the header component to be used for all pages
+// it has three elements:
+// 1. the left arrow that navigates to the previous page (always present)
+// 2. the title of the page ('children' props)
+// 3. the little red heart (invisible by default, if you want it visible, pass the hasHeartButton={true} prop. If you want some function to be called when the heart is clicked, eg. one that adds the movie to favorites, pass it as onHeartButtonClick prop)
+// it has its own padding, so take care it does not get additional vertical and horizontal padding from your component
+
 function HeaderPage(props: Props) {
   const [isLiked, setIsLiked] = useState(false);
   const navigation = useNavigate();
