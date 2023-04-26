79c9e1af5a73ff3e93e6da7ce4a7d981e3fe9059
ehidne-mora <mora@slime.garden>
2023-04-26 15:32:43 +0200

    added more comments to the HeaderPage.tsx

--- src/components/HeaderPage.tsx
+++ src/components/HeaderPage.tsx
@@ -2,11 +2,12 @@ import { ChevronLeftIcon, HeartIcon } from '@heroicons/react/24/solid';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-interface Props {
+// type alias for the props
+type Props = {
   children: string;
   hasHeartButton?: boolean;
   onHeartButtonClick?: () => void;
-}
+};
 
 // This is the header component to be used for all pages
 // it has three elements:
@@ -16,14 +17,20 @@ interface Props {
 // it has its own padding, so take care it does not get additional vertical and horizontal padding from your component
 
 function HeaderPage(props: Props) {
+  // state that toggles if the little heart is displayed as full or empty
   const [isLiked, setIsLiked] = useState(false);
+  // function used to navigate back to the last page when the arrow back is clicked
   const navigation = useNavigate();
+  // the return statement with the jsx component
   return (
     <header className="flex justify-between items-center px-5 pb-6 pt-8 sticky top-0 bg-dark">
+      {/* the little back arrow */}
       <button onClick={() => navigation(-1)}>
         <ChevronLeftIcon className="typography-title w-5"></ChevronLeftIcon>
       </button>
+      {/* the title of the page */}
       <h1 className="typography-title">{props.children}</h1>
+      {/* the heart icon, visible if the hasHeartButton={true} prop is passed, can call the callback function on click if one is passed as the onHeartButtonClick prop */}
       <div className="w-5 h-5">
         {props.hasHeartButton && (
           <button
