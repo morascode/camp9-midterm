0c0ff00d62dddba6fa39414700c052dd6a09a392
estefaninoely <estefaninoely@gmail.com>
2023-04-27 15:34:38 +0200

    edits Files

--- src/components/accountpage/EditProfil.tsx
+++ src/components/accountpage/EditProfil.tsx
@@ -1,8 +1,14 @@
 import Button from '../Button';
 import MovieDetailHeader from '../HeaderPages';
+import EditProfilInputForm from './EditProfileForm';
 
 function EditProfil() {
-  return <div></div>;
+  return (
+    <div>
+      <MovieDetailHeader>Edit Profil</MovieDetailHeader>
+      <EditProfilInputForm />
+    </div>
+  );
 }
 
 export default EditProfil;
