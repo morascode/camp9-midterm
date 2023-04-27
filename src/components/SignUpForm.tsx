c48fb737c571a6d2ad2dedf2d35dc686ac1ae070
lui <quenaya@web.de>
2023-04-27 11:31:42 +0200

    missing last task

---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Checkbox from './Checkbox';
+import SingleInputFieldLogIn from './SingleInputFieldLogIn';
+import { useState } from 'react';
+import Radio from './Radio';
+
+type SignUpForm = React.FormHTMLAttributes<HTMLFormElement>;
+
+function SignUpForm() {
+  const [selectedOption, setSelectedOption] = useState('');
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+  };
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  return (
+    <>
+      <form action="" onSubmit={() => {}}>
+        <SingleInputFieldLogIn
+          placeholder={'your@email.com'}
+          svg={'email'}
+          type="email"
+        ></SingleInputFieldLogIn>
+
+        <SingleInputFieldLogIn
+          placeholder={'Enter your Password'}
+          svg={'key'}
+          type="password"
+        ></SingleInputFieldLogIn>
+        <div className=" flex flex-row justify-around">
+          <div className="flex flex-col">
+            <Radio
+              name="age"
+              label="I'm 18"
+              value="I'm 18"
+              checked={false}
+              onChange={handleRadioChange}
+            ></Radio>
+            <Radio
+              name="age"
+              label="I'm not 18"
+              value="I'm not 18"
+              checked={false}
+              onChange={handleRadioChange}
+            ></Radio>
+          </div>
+          <Checkbox
+            label="student"
+            name="student"
+            checked={false}
+            onChange={handleCheckboxChange}
+          ></Checkbox>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default SignUpForm;
--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,23 +1,65 @@
 import { useState } from 'react';
-import Button from '../components/Button';
+import ToggleButton from '../components/ToggleButton';
 import LogInForm from '../components/LogInForm';
 import { Link } from 'react-router-dom';
+import Button from '../components/Button';
+import SignUpForm from '../components/SignUpForm';
 
 function LogInPage() {
-  const [paths, setPaths] = useState('/login');
+  const [LogInOrSignUp, SetLogInOrSignUp] = useState<'LogIn' | 'SignUp'>(
+    'LogIn'
+  );
 
   return (
-    <div className="px-5 py-8 h-screen relative flex flex-col">
-      <h1 className="typography-title">Welcome to Cine-Scape</h1>
-      <p className="typography-body pt-3 pb-5">
-        You need to log in to be able to make reservations and add movies to
-        your watchlist.
-      </p>
-      <LogInForm></LogInForm>
-      <div className='flex-auto'></div>
-      <Link to="/" className=" w-full">
-        <Button className="typography-primary">Login</Button>
-      </Link>
+    <div className="px-5 py-8 h-screen relative flex justify-between flex-col">
+      {LogInOrSignUp === 'LogIn' ? (
+        <div>
+          <h1 className="typography-title">Welcome to Cine-Scape</h1>
+          <p className="typography-body pt-3 pb-5">
+            You need to log in to be able to make reservations and add movies to
+            your watchlist.
+          </p>
+          <LogInForm></LogInForm>
+          <div className="my-10"></div>
+          <Link to="/home">
+            <Button size="md">Log In</Button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <h1 className="typography-title">Welcome to Cine-Scape</h1>
+          <p className="typography-body pt-3 pb-5">
+            You need to sign-up to be able to make reservations and add movies
+            to your watchlist.
+          </p>
+          <SignUpForm></SignUpForm>
+          <div className="my-10"></div>
+          <Link to="/home">
+            <Button size="md">Sign Up</Button>
+          </Link>
+        </div>
+      )}
+
+      <div className="flex justify-between">
+        <ToggleButton
+          status={LogInOrSignUp === 'LogIn' ? 'active' : 'passive'}
+          onClick={() => {
+            SetLogInOrSignUp('LogIn');
+          }}
+          className="typography-primary"
+        >
+          Login
+        </ToggleButton>
+        <ToggleButton
+          onClick={() => {
+            SetLogInOrSignUp('SignUp');
+          }}
+          status={LogInOrSignUp === 'SignUp' ? 'active' : 'passive'}
+          className="typography-primary"
+        >
+          Sign Up
+        </ToggleButton>
+      </div>
     </div>
   );
 }
