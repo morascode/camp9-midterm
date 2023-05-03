54446e5fd3fa18725b5e247eb1202ec13ca47549
ehidne-mora <mora@slime.garden>
2023-05-03 13:58:49 +0200

    finished working on the useGetInfiniteMovies hook, also added comments

--- client/src/hooks/useGetInfiniteMovies.ts
+++ client/src/hooks/useGetInfiniteMovies.ts
@@ -3,21 +3,18 @@ import axios from 'axios';
 import { MovieDbResponse } from '../utilities/types';
 import { EmojieLibraryEntry, useEmojieLibrary } from '../contexts/GenreContext';
 
+// this funciton just returns an array of IDs of currently selected genres from the GenreContext
 function getSelectedGenres(filteredEmojieLibrary: EmojieLibraryEntry[]) {
-  const selectedGenreIDs = filteredEmojieLibrary
+  const selectedGenresIDs = filteredEmojieLibrary
     .filter(genreid => {
       if (genreid.isSelected === true) return genreid.GenreId;
     })
     .map(genreid => genreid.GenreId);
-  if (selectedGenreIDs.length <= 0) {
-    return '';
-  } else {
-    let selectedGenresParameter = '&with_genres=';
-    selectedGenresParameter += selectedGenreIDs.join('|');
-    return selectedGenresParameter;
-  }
+  return selectedGenresIDs;
 }
 
+// this function acts as the query function for useGetInfiniteMovies hook
+// it fetches 20 movies from the given page with selected gernes
 async function getInfiniteMovies(
   pageNumber: number,
   selectedGenresParameter: string
@@ -31,15 +28,22 @@ async function getInfiniteMovies(
   return response.data;
 }
 
+// this hook gets 20 movies per fetch
+// returns the query object including:
+// data.pages - an array of all pages fetched, 20 movies per page (up to 500 pages)
+// fetchNextPage - function that fetches the next 20 movies, saves them in data.pages (note: do not pass any arguments to this function!)
+// hasNextPage - boolean determining if the next page can be fetched
 export function useGetInfiniteMovies() {
   const { filteredEmojieLibrary } = useEmojieLibrary();
-  const selectedGenresParameter = getSelectedGenres(filteredEmojieLibrary);
+  const selectedGenres = getSelectedGenres(filteredEmojieLibrary);
   const infiniteQuery = useInfiniteQuery({
-    queryKey: ['infiniteMovies'],
+    queryKey: ['infiniteMovies', { genres: selectedGenres }],
     queryFn: ({ pageParam = 1 }) =>
-      getInfiniteMovies(pageParam, selectedGenresParameter),
-    getNextPageParam: (lastPage, allPages) =>
-      lastPage.page == 500 ? undefined : lastPage.page + 1,
+      getInfiniteMovies(pageParam, `&with_genres=${selectedGenres.join('|')}`),
+    getNextPageParam: lastPage =>
+      lastPage.page >= lastPage.total_pages || lastPage.page >= 500
+        ? undefined
+        : lastPage.page + 1,
   });
   return infiniteQuery;
 }
