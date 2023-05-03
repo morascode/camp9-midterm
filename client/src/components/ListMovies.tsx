13c54be87e4d4ddcbecf57700381496f837f1285
lui <quenaya@web.de>
2023-05-03 14:53:23 +0200

    infinityscroll working

--- client/src/components/ListMovies.tsx
+++ client/src/components/ListMovies.tsx
@@ -1,45 +1,50 @@
 import { Link } from 'react-router-dom';
-import { useEmojieMovies } from '../hooks/useEmojieSorting';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { useGetInfiniteMovies } from '../hooks/useGetInfiniteMovies';
 
 export default function ListMovies() {
-  const { isError, isLoading, movies } = useEmojieMovies();
-
+  const { isError, isLoading, data, fetchNextPage, hasNextPage } =
+    useGetInfiniteMovies();
+  console.log(data);
   if (isError) {
     return <h1>"Couldn't find the movies, sorry"</h1>;
-  }
-  if (isLoading) {
+  } else if (isLoading) {
     return <h1>'wait a sec...'</h1>;
-  }
-  const twentyMovies = movies!;
-
-  return (
-    <div>
-      <InfiniteScroll
-        className="grid grid-rows-2 grid-cols-2 gap-5"
-        dataLength={twentyMovies.length}
-        next={() => {
-          console.log('Hai new movies');
-        }}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: 'center' }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-      >
-        {twentyMovies?.map((movie, index) => {
-          let image = movie.poster_path;
-          return (
-            <Link to={`/movies/${movie.id}`} key={index}>
-              <div className="h-auto">
-                <img src={`https://image.tmdb.org/t/p/original/${image}`}></img>
+  } else
+    return (
+      <div>
+        <InfiniteScroll
+          dataLength={data.pages.length}
+          next={() => {
+            fetchNextPage();
+          }}
+          hasMore={!!hasNextPage}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          {data.pages.map(page => {
+            return (
+              <div className="grid grid-rows-2 grid-cols-2 gap-5">
+                {page.results.map((movie, index) => {
+                  let image = movie.poster_path;
+                  return (
+                    <Link to={`/movies/${movie.id}`} key={index}>
+                      <div className="h-auto">
+                        <img
+                          src={`https://image.tmdb.org/t/p/original/${image}`}
+                        ></img>
+                      </div>
+                    </Link>
+                  );
+                })}
               </div>
-            </Link>
-          );
-        })}
-      </InfiniteScroll>
-    </div>
-  );
+            );
+          })}
+        </InfiniteScroll>
+      </div>
+    );
 }
