f367f7091a83681a03e81aa99c6ea168bd7804df
Doug <dmhwebb@gmail.com>
2023-05-04 16:44:14 +0200

    seat type console logged on press

--- client/src/components/Seat.tsx
+++ client/src/components/Seat.tsx
@@ -10,9 +10,11 @@ export type SeatSection = 'front' | 'middle' | 'back';
 
 export function Seat(props: Props) {
   const [selected, setSelected] = useState(false);
+  const { type } = props;
 
   return (
     <button
+      id={type}
       disabled={false}
       className={clsx(
         'rounded-sm h-7 w-7',
@@ -26,6 +28,7 @@ export function Seat(props: Props) {
         switch (selected) {
           case false:
             setSelected(true);
+            console.log(e.target.id);
             break;
           case true:
             setSelected(false);
