6e9799e2f803adf2c776fff2ffb3bb690d4e66fa
mateusmorgado <mateusmorgado@hotmail.com>
2023-05-04 16:41:59 +0200

    editing popup

--- client/src/components/SeatPopover.tsx
+++ client/src/components/SeatPopover.tsx
@@ -5,9 +5,34 @@ import Button from './Button';
 function SeatPopover() {
   return (
     <>
-      <div className="bg-[#494952] rounded-[12px] w-full pt-7 pb-5 px-5">
-        <div></div>
-        <hr className='white-dimmed-heavy ' />
+      <div className="bg-[#494952] rounded-[12px] w-full pt-7 pb-5 px-5 fixed bottom-0">
+        <div className="grid">
+          <div className="flex  justify-between typography-primary">
+            <div>
+              1x &nbsp;&nbsp;<span className="typography-title text-sm "> Seat - Front</span>
+            </div>
+            <div>
+              <span className="typography-title text-sm">$12.44</span>/ each
+            </div>
+          </div>
+          <div className="flex justify-between typography-primary">
+            <div>
+              2x &nbsp;&nbsp;<span className="typography-title text-sm">Seat - Front</span>
+            </div>
+            <div>
+              <span className="typography-title text-sm">$12.44</span>/ each
+            </div>
+          </div>
+          <div className="flex justify-between typography-primary">
+            <div>
+              2x &nbsp;&nbsp;<span className="typography-title text-sm">Seat - Front</span>
+            </div>
+            <div>
+              <span className="typography-title text-sm">$12.44</span>/ each
+            </div>
+          </div>
+        </div>
+        <hr className="white-dimmed-heavy mt-4 " />
         <div className="flex justify-between items-end mt-7 ">
           <div className="grid typography-title text-2xl">
             <span className="typography-description ">Total Price</span>43,50
