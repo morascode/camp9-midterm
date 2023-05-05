ed07a2604075ee65cafe2a91085529e7b8d01845
Doug <dmhwebb@gmail.com>
2023-05-05 14:16:41 +0200

    price and seat number updates on selection. Math issues + seats re-renders each time

--- client/src/components/SeatPopover.tsx
+++ client/src/components/SeatPopover.tsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom';
 import Button from './Button';
 
-function SeatPopover() {
+interface Props {
+  seatObject: { front: number; middle: number; back: number };
+}
+
+function SeatPopover(props: Props) {
+  const { seatObject } = props;
   return (
     <>
       <div className="bg-[#494952] rounded-[12px] w-full pt-7 pb-5 px-5 fixed bottom-0">
         <div className="grid">
           <div className="flex  justify-between typography-primary">
             <div>
-              1x
+              {seatObject.front} X
               <span className="typography-title text-sm "> Seat - Front</span>
             </div>
             <div>
@@ -17,7 +22,7 @@ function SeatPopover() {
           </div>
           <div className="flex justify-between typography-primary">
             <div>
-              2x
+              {seatObject.middle} X
               <span className="typography-title text-sm"> Seat - Middle</span>
             </div>
             <div>
@@ -26,7 +31,7 @@ function SeatPopover() {
           </div>
           <div className="flex justify-between typography-primary">
             <div>
-              2x
+              {seatObject.back} X
               <span className="typography-title text-sm"> Seat - Back</span>
             </div>
             <div>
@@ -37,7 +42,7 @@ function SeatPopover() {
         <hr className="white-dimmed-heavy mt-4 " />
         <div className="flex justify-between items-end mt-7 ">
           <div className="grid typography-title text-2xl">
-            <span className="typography-description ">Total Price</span>43,50
+            <span className="typography-description ">Total Price</span>$ {seatObject.front * 12.95 + seatObject.middle * 14.95 + seatObject.back * 16.95}
           </div>
           <Link className="w-[215px]" to={'/success'}>
             <Button className="">Book Ticket</Button>
