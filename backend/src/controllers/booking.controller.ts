ac226ce598dec431601ff599644beb1fe5a9dfb1
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-05-08 16:01:46 +0200

    moved booking routes to different file

--- backend/src/controllers/booking.controller.ts
+++ backend/src/controllers/booking.controller.ts
@@ -59,5 +59,5 @@ export const bookingController = async (
       }, 0),
     },
   });
-  res.send(booking);
+  res.status(201).send(booking);
 };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,12 +1,10 @@
+import { userValidation } from '../validate/userValidation';
 import { Router } from 'express';
 import { signupController } from '../controllers/user.controllers';
-import { z } from 'zod';
 import { validate } from '../middleware/validateResource';
-import { userValidation } from '../validate/uservalidation';
-import { loginValidation } from '../validate/loginvalidation';
+import { loginValidation } from '../validate/loginValidation';
 import { loginController } from '../controllers/login.controller';
-import { bookingController } from '../controllers/booking.controller';
-import { bookingValidation } from '../validate/bookingValidation';
+
 const router = Router();
 
 //@route POST /api/1.0/user/signup
@@ -20,11 +18,5 @@ router.post('/signup', validate(userValidation), signupController);
 //@access Public
 
 router.post('/login', validate(loginValidation), loginController);
-
-//@route POST /api/1.0/user/booking
-//@desc booking movie by user
-//@access Public // private ?
-
-router.post('/booking', validate(bookingValidation), bookingController);
 
 export default router;
