cda16a51c5402f99bd97856a0c4c4db147b589df
Manuel Pastor Ringuelet <pastorringuelet@gmail.com>
2023-05-09 14:39:05 +0200

    removed unnecessaty code in BookmarkedMovies

--- frontend/src/pages/BookmarkedMovies.tsx
+++ frontend/src/pages/BookmarkedMovies.tsx
@@ -1,15 +1,11 @@
 import PaginationBookmarkedMovies from '../components/PaginationBookmarkedMovies';
-import { useState } from 'react';
-import PaginationButtons from '../components/PaginationButtons';
 
 export default function BookmarkedMovies() {
-  const [state, setState] = useState(1);
 
   return (
     <>
       <div className="px-5 py-8">
         <PaginationBookmarkedMovies />
-        <PaginationButtons state={state} setState={setState} />
       </div>
     </>
   );
